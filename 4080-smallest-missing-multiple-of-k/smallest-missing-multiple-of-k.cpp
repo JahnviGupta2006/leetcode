5a872eed65284ab68bba57650f6a2e32ba5324be
Jahnvi Gupta <jahnvigupta2006@gmail.com>
2026-08-26 00:12:38 +0530

    Time: 3 ms (38.06%) | Memory: 25.5 MB (24.36%) - LeetSync

---
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int find(vector<int>nums,int i,int num){
+        for(int j=i;j<nums.size();j++){
+            if(nums[j]==num)return j;
+        }
+        return -1;
+    }
+    int missingMultiple(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        int i=0;
+        int org=k;
+        int j=1;
+        while(find(nums,i,k)!=-1){
+           
+            i=find(nums,i,k);
+            j++;
+            k=org*j;
+        }
+        if(find(nums,i,k)==-1)return k; 
+        return i;
+    }
+};
